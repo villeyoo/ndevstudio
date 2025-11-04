49a98ddb687dcc28bbc1180bf8b0fadc3c98319d
villeyoo <nabilmaulanaaalhakim@gmail.com>
2025-11-05 06:14:42 +0700

    Tess

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,13 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-       // middleware bawaan
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
     
