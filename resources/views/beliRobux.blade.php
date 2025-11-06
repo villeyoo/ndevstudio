a576c81f82a88b38b9360eef78db0be6394410a7
villeyoo <nabilmaulanaaalhakim@gmail.com>
2025-11-06 22:31:50 +0700

    update

--- resources/views/beliRobux.blade.php
+++ resources/views/beliRobux.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NDEV STUDIO - BELI ROBUX</title>
   <link rel="icon" type="image/png" href="{{ asset('assets/images/viel.png') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/Belirobux.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/beliobux.css') }}">
   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
