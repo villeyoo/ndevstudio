2e1cef0352128860cf357e1b24d8bbf43877e8cf
villeyoo <nabilmaulanaaalhakim@gmail.com>
2026-01-16 01:27:32 +0700

    update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Models\Laporan;
+use Illuminate\Support\Facades\Storage;
+
 
 class ReportController extends Controller
 {
@@ -31,32 +33,12 @@ class ReportController extends Controller
 
         $path = null;
 
-
         if ($request->hasFile('attachment')) {
-            $file = $request->file('attachment');
-
-            if ($file && $file->isValid()) {
-
-                $folder = storage_path('app/public/laporan');
-                if (!is_dir($folder)) {
-                    mkdir($folder, 0755, true);
-                }
-
-                $filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
-                $file->move($folder, $filename);
-
-                $path = 'laporan/' . $filename;
-            }
+            $path = $request->file('attachment')->store('laporan', 'public');
         }
 
-        // =========================
-        // GENERATE TOKEN
-        // =========================
         $token = strtoupper(Str::random(10));
 
-        // =========================
-        // SIMPAN KE DATABASE
-        // =========================
         Laporan::create([
             'nama'       => $request->nama,
             'kontak'     => $request->kontak,
@@ -67,15 +49,12 @@ class ReportController extends Controller
             'status'     => 'DITERIMA',
         ]);
 
-        // =========================
-        // KIRIM KE SESSION
-        // =========================
         return back()
             ->with('success', 'Laporan berhasil dikirim!')
             ->with('token', $token);
     }
 
-     public function cekForm()
+    public function cekForm()
     {
         return view('ceklaporan');
     }
